aecde8ccf7efa298036d694cfb1508926f3d92e5
Sri Lakshmi <srijish0609@gmail.com>
2026-05-30 15:27:24 +0530

    Initial commit

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from langchain_community.document_loaders import PyPDFLoader
-from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
